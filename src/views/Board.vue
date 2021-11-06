f59e770d150b6aa8f22d357dc42d6bd87a3390ef
Zarin Loosli <zarinloosli@gmail.com>
2021-11-06 16:58:05 -0600

    Add grid

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -4,6 +4,15 @@
       This is the board, please login to play
     </h1>
     <h1 v-else>{{ this.$root.$data.username }}'s Board</h1>
+    <div>
+      <div>
+        <table>
+          <tr v-for="i in 15" :key="i">
+            <td v-for="j in 15" :key="j">{{ i }} {{ j }}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
