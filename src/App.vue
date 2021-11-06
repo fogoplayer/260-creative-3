19e731859d4d3b56cb0ea53ae798337ceef9eee9
Jacob McKenzie <jacobmckenzie46@gmail.com>
2021-11-06 14:54:00 -0600

    Updated Logo, Header, Footer

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,19 @@
       <router-link to="/login">Login</router-link> |
       <router-link to="/board">Board</router-link>
     </div>
-    <router-view />
+    <hr />
+    <!-- This is where the component from the router will be injected you can put a footer under it too-->
+    <div id="content-wrap">
+      <router-view />
+    </div>
+
+    <footer class="footer">
+      <p>
+        &copy; 2021 Jacob McKenzie & Zarin Loosli, Inc. &middot;
+        <a href="https://github.com/fogoplayer/260-creative-3">Repository</a> |
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
   </div>
 </template>
 
@@ -16,18 +28,41 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  position: relative;
+  min-height: 100%;
 }
 
 #nav {
-  padding: 30px;
+  padding: 20px;
+  font-size: 30px;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  border-radius: 5px;
+  padding: 4px;
+}
+
+#nav a:hover {
+  color: white;
+  background-color: rgb(66, 66, 66);
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  /* color: #42b983; */
+  color: white;
+  background-color: black;
+}
+
+#footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 2.5rem; /* Footer height */
+}
+#content-wrap {
+  padding-bottom: 2.5rem; /* Footer height */
 }
 </style>
