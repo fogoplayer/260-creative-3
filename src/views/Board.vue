f76f0e7ae90d16ab5c6827ada4eac1befb643fd3
Zarin Loosli <zarinloosli@gmail.com>
2021-11-06 16:52:09 -0600

    Copy over work

---
+++ src/views/Board.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1 v-if="this.$root.$data.username == ''">
+      This is the board, please login to play
+    </h1>
+    <h1 v-else>{{ this.$root.$data.username }}'s Board</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Board",
+};
+</script>
