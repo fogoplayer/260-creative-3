dab316f58438802fbee3b70664916665dd43fb2b
Zarin Loosli <zarinloosli@gmail.com>
2021-12-04 01:44:39 -0700

    Finish styling profile

--- front-end/src/views/Profile.vue
+++ front-end/src/views/Profile.vue
@@ -1,18 +1,26 @@
 <template>
   <form @submit.prevent="">
-    <h1><input class="username" :value="this.$root.$data.username" /></h1>
-    High Score: {{ this.$root.$data.highScore }} Games Played:
-    {{ this.$root.$data.gamesPlayed }}
-    <img :src="this.$root.$data.path" />
+    <input class="username" :value="this.$root.$data.username" />
+    <section class="stats">
+      High Score: {{ this.$root.$data.highScore }}<br />
+      Games Played:
+      {{ this.$root.$data.gamesPlayed }}
+    </section>
+    <img class="profile-picture" :src="this.$root.$data.path" />
     <fieldset>
       <label>
-        Password <input type="password" :value="this.$root.$data.password"
+        Password
+        <input
+          class="password"
+          type="password"
+          :value="this.$root.$data.password"
       /></label>
-      <label>
-        Bio
-        <textarea v-model="this.$root.$data.bio"> </textarea>
-      </label>
-      <button>Delete Account</button>
+      <label for="bio"> Bio</label>
+      <textarea id="bio" class="bio" v-model="this.$root.$data.bio"> </textarea>
+      <div class="btn-container">
+        <button class="save">Save changes</button>
+        <button class="delete">Delete Account</button>
+      </div>
     </fieldset>
   </form>
 </template>
@@ -26,13 +34,9 @@ export default {
 form {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 500px));
+  gap: 1rem;
+  justify-content: space-evenly;
   margin: auto;
-}
-
-fieldset {
-  border: none;
-  display: flex;
-  flex-flow: column nowrap;
 }
 
 input {
@@ -40,7 +44,54 @@ input {
   border-bottom: 1px solid gray;
 }
 
-img {
-  width: 500px;
+.username {
+  font-size: 2em;
+  margin: none;
+  align-self: end;
+}
+
+.stats {
+  align-self: end;
+}
+
+.profile-picture {
+  aspect-ratio: 1/1;
+  object-fit: cover;
+  width: 100%;
+  max-width: 200px;
+  border-radius: 50%;
+  justify-self: center;
+}
+
+fieldset {
+  border: none;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: flex-start;
+  gap: 0.5rem;
+}
+
+.bio {
+  display: block;
+  width: 100%;
+}
+
+.btn-container {
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+}
+
+button {
+  border-radius: 0;
+  padding: 0.5em 1em;
+}
+
+.save {
+  background: lightgreen;
+}
+
+.delete {
+  background: #ffaaaa;
 }
 </style>
