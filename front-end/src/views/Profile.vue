6b7dcfd3d56bf676778b1a3aaac077bb603e7964
Zarin Loosli <zarinloosli@gmail.com>
2021-12-03 23:21:18 -0700

    Create all fields for profile page

--- front-end/src/views/Profile.vue
+++ front-end/src/views/Profile.vue
@@ -1,4 +1,17 @@
-<template><div>Hello world</div></template>
+<template>
+  <div>
+    <input class="username" :value="this.$root.$data.username" />
+    High Score: {{ this.$root.data.highScore }} Games Played:
+    {{ this.$root.data.gamesPlayed }}
+    <img :src="this.$root.data.path" />
+    <input :value="this.$root.data.path" />
+    <input type="password" :value="this.$root.data.password" />
+    <label>
+      <textarea v-model="this.$root.data.bio"> </textarea>
+    </label>
+    <button>Delete Account</button>
+  </div>
+</template>
 <script>
 export default {
   name: "Profile",
