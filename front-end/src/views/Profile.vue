82d644fecb3d3a11bcd9415e9d27a9559ccae643
Zarin Loosli <zarinloosli@gmail.com>
2021-12-04 01:10:12 -0700

    Make profile page into a grid

--- front-end/src/views/Profile.vue
+++ front-end/src/views/Profile.vue
@@ -1,19 +1,46 @@
 <template>
-  <div>
-    <input class="username" :value="this.$root.$data.username" />
+  <form @submit.prevent="">
+    <h1><input class="username" :value="this.$root.$data.username" /></h1>
     High Score: {{ this.$root.$data.highScore }} Games Played:
     {{ this.$root.$data.gamesPlayed }}
     <img :src="this.$root.$data.path" />
-    <input :value="this.$root.$data.path" />
-    <input type="password" :value="this.$root.$data.password" />
-    <label>
-      <textarea v-model="this.$root.$data.bio"> </textarea>
-    </label>
-    <button>Delete Account</button>
-  </div>
+    <fieldset>
+      <label>
+        Password <input type="password" :value="this.$root.$data.password"
+      /></label>
+      <label>
+        Bio
+        <textarea v-model="this.$root.$data.bio"> </textarea>
+      </label>
+      <button>Delete Account</button>
+    </fieldset>
+  </form>
 </template>
 <script>
 export default {
   name: "Profile",
 };
 </script>
+
+<style scoped>
+form {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 500px));
+  margin: auto;
+}
+
+fieldset {
+  border: none;
+  display: flex;
+  flex-flow: column nowrap;
+}
+
+input {
+  border: none;
+  border-bottom: 1px solid gray;
+}
+
+img {
+  width: 500px;
+}
+</style>
